a03f1ffe5cbdefcd7cd3dc2c7234b904362e0913
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-05-24 12:14:02 +0200

    nav

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -3,126 +3,142 @@
 </script>
 
 <header>
+	<div class="sm:flex place-self-end gap-12 px-20 pt-2 place-content-end hidden ">
+		<a sveltekit:prefetch href="/"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				aria-hidden="true"
+				role="img"
+				class="iconify iconify--ph"
+				width="20"
+				height="20"
+				preserveAspectRatio="xMidYMid meet"
+				viewBox="0 0 256 256"
+				><path
+					fill="black"
+					d="M94 216a14 14 0 1 1-14-14a14 14 0 0 1 14 14Zm90-14a14 14 0 1 0 14 14a14 14 0 0 0-14-14Zm43.5-128.4L201.1 166a22.2 22.2 0 0 1-21.2 16H84.1a22.2 22.2 0 0 1-21.2-16L36.5 73.8v-.3l-9.8-34a1.9 1.9 0 0 0-1.9-1.5H8a6 6 0 0 1 0-12h16.8a14.1 14.1 0 0 1 13.5 10.2L46.8 66h174.9a6 6 0 0 1 4.8 2.4a6 6 0 0 1 1 5.2ZM213.8 78H50.2l24.3 84.7a10 10 0 0 0 9.6 7.3h95.8a10 10 0 0 0 9.6-7.3Z"
+				/></svg
+			></a
+		>
+		<a sveltekit:prefetch href="/" class="text-decoration-underline: no-underline text-[0.7rem]"
+			>LOG IND</a
+		>
+	</div>
 	<nav>
 		<div class="desktop-menu">
 			<ul>
-				<div class="dropdown">
-					<a href="/" class="dropbtn"
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							xmlns:xlink="http://www.w3.org/1999/xlink"
-							aria-hidden="true"
-							role="img"
-							class="iconify iconify--material-symbols"
-							width="20"
-							height="20"
-							preserveAspectRatio="xMidYMid meet"
-							viewBox="0 0 24 24"
-							><path
-								fill="#444555"
-								d="M7 22q-.825 0-1.412-.587Q5 20.825 5 20q0-.825.588-1.413Q6.175 18 7 18t1.412.587Q9 19.175 9 20q0 .825-.588 1.413Q7.825 22 7 22Zm10 0q-.825 0-1.412-.587Q15 20.825 15 20q0-.825.588-1.413Q16.175 18 17 18t1.413.587Q19 19.175 19 20q0 .825-.587 1.413Q17.825 22 17 22ZM3.625 17L6.6 11.6L3 4H1V2h3.25l.95 2h16.5l-4.975 9H8.1L7 15h12v2Z"
-							/></svg
-						>SHOP</a
-					>
-					<div class="dropdown-content">
-						<a sveltekit:prefetch href="/">SHOP ØL</a>
-						<a sveltekit:prefetch href="/">KØB GAVEKORT</a>
-					</div>
-				</div>
-
-				<li class:active={$page.url.pathname === '/'}>
-					<a sveltekit:prefetch href="/">BOOK BORD</a>
-				</li>
-
-				<li class:active={$page.url.pathname === '/'}>
-					<a sveltekit:prefetch href="/">MENUKORT</a>
-				</li>
-
-				<div class="dropdown">
-					<a href="/" class="dropbtn">VORES ØLUDVALG</a>
-					<div class="dropdown-content">
-						<a sveltekit:prefetch href="/">NØRREBRO BRYGHUS</a>
-						<a sveltekit:prefetch href="/">BRAW</a>
-						<a sveltekit:prefetch href="/">INFERNØL</a>
-						<a sveltekit:prefetch href="/">ØL HEMMELIGHEDER</a>
-					</div>
-				</div>
-
-				<!-- <li class:active={$page.url.pathname === '/'}> -->
-
-				<div class="">
+				<div class="pr-[16.5rem]">
 					<li class:active={$page.url.pathname === '/'}>
 						<a sveltekit:prefetch href="/"><img src="nb_logo.svg" alt="" /></a>
 					</li>
 				</div>
 
-				<div class="dropdown">
-					<a href="/" class="dropbtn">EVENTS & SELSKABER</a>
-					<div class="dropdown-content">
-						<a sveltekit:prefetch href="/">EVENTKALENDER</a>
-						<a sveltekit:prefetch href="/">SELSKABER & MØDELOKALER</a>
+				<div class="">
+					<div class="dropdown">
+						<a href="/" class="dropbtn">SHOP ⯆</a>
+						<div class="dropdown-content">
+							<a sveltekit:prefetch href="/">ØL-SHOP</a>
+							<a sveltekit:prefetch href="/">GAVEKORT</a>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<div class:active={$page.url.pathname === '/'}>
+							<a sveltekit:prefetch href="/">MENUKORT</a>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<div class:active={$page.url.pathname === '/'}>
+							<a sveltekit:prefetch href="/">BOOK BORD</a>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<a href="/" class="dropbtn">EVENTS & SELSKABER ⯆</a>
+						<div class="dropdown-content">
+							<a sveltekit:prefetch href="/">EVENTKALENDER</a>
+							<a sveltekit:prefetch href="/">SELSKABER & MØDELOKALER</a>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<a href="/" class="dropbtn">VORES ØLUDVALG ⯆</a>
+						<div class="dropdown-content">
+							<a sveltekit:prefetch href="/">NØRREBRO BRYGHUS</a>
+							<a sveltekit:prefetch href="/">BRAW</a>
+							<a sveltekit:prefetch href="/">INFERNØL</a>
+							<a sveltekit:prefetch href="/">ØL HEMMELIGHEDER</a>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<a href="/" class="dropbtn">OM BRYGHUSET ⯆</a>
+						<div class="dropdown-content">
+							<a sveltekit:prefetch href="/">VORES HISTORIE</a>
+							<a sveltekit:prefetch href="/">BÆREDYGTIGHED</a>
+							<a sveltekit:prefetch href="/">OM BRAW BAR</a>
+							<a sveltekit:prefetch href="/">ØLFORUM</a>
+							<a sveltekit:prefetch href="/">NYHEDER FRA BRYGHUSET</a>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<a href="/" class="dropbtn">FOR VIRKSOMHEDER ⯆</a>
+						<div class="dropdown-content">
+							<a sveltekit:prefetch href="/">FIRMAEVENTS</a>
+							<a sveltekit:prefetch href="/">BOOK ET MØDELOKALE</a>
+							<a sveltekit:prefetch href="/">BLIV FORHANDLER</a>
+						</div>
 					</div>
 				</div>
-
-				<div class="dropdown">
-					<a href="/" class="dropbtn">OM BRYGHUSET</a>
-					<div class="dropdown-content">
-						<a sveltekit:prefetch href="/">VORES HISTORIE</a>
-						<a sveltekit:prefetch href="/">BÆREDYGTIGHED</a>
-						<a sveltekit:prefetch href="/">OM BRAW BAR</a>
-						<a sveltekit:prefetch href="/">ØLFORUM</a>
-						<a sveltekit:prefetch href="/">NYHEDER FRA BRYGHUSET</a>
-					</div>
-				</div>
-
-				<!-- </li> -->
-
-				<!-- <li class:active={$page.url.pathname === '/'}> -->
-
-				<div class="dropdown">
-					<a href="/" class="dropbtn">FOR VIRKSOMHEDER</a>
-					<div class="dropdown-content">
-						<a sveltekit:prefetch href="/">FIRMAEVENTS</a>
-						<a sveltekit:prefetch href="/">BOOK ET MØDELOKALE</a>
-						<a sveltekit:prefetch href="/">BLIV FORHANDLER</a>
-					</div>
-				</div>
-
-				<!-- </li> -->
 			</ul>
 		</div>
 
 		<!-----------HAMBURGER MENU----------->
 
-		<div class="hamburger-menu">
-			<a sveltekit:prefetch href="/"><img src="nb_logo.svg" alt="" /></a>
+		<div class="hamburger-menu flex pr-20">
+			<a class=" " sveltekit:prefetch href="/"><img src="nb_logo.svg" alt="" /></a>
+
+			<div class="flex pl-36">
+				<a sveltekit:prefetch href="/"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						aria-hidden="true"
+						role="img"
+						class="iconify iconify--ph"
+						width="20"
+						height="20"
+						preserveAspectRatio="xMidYMid meet"
+						viewBox="0 0 256 256"
+						><path
+							fill="black"
+							d="M94 216a14 14 0 1 1-14-14a14 14 0 0 1 14 14Zm90-14a14 14 0 1 0 14 14a14 14 0 0 0-14-14Zm43.5-128.4L201.1 166a22.2 22.2 0 0 1-21.2 16H84.1a22.2 22.2 0 0 1-21.2-16L36.5 73.8v-.3l-9.8-34a1.9 1.9 0 0 0-1.9-1.5H8a6 6 0 0 1 0-12h16.8a14.1 14.1 0 0 1 13.5 10.2L46.8 66h174.9a6 6 0 0 1 4.8 2.4a6 6 0 0 1 1 5.2ZM213.8 78H50.2l24.3 84.7a10 10 0 0 0 9.6 7.3h95.8a10 10 0 0 0 9.6-7.3Z"
+						/></svg
+					></a
+				>
+				<a sveltekit:prefetch href="/" class="text-decoration-underline: no-underline text-[0.7rem]"
+					>LOG IND</a
+				>
+			</div>
+
 			<input id="menu__toggle" type="checkbox" />
 			<label class="menu__btn" for="menu__toggle">
 				<span />
 			</label>
 
 			<ul class="menu__box">
+				<li class:active={$page.url.pathname === '/'}>
+					<a sveltekit:prefetch href="/"><img src="nb_logo.svg" alt="" /></a>
+				</li>
+
 				<li>
 					<div class="dropdown">
-						<div class="dropbtn">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								xmlns:xlink="http://www.w3.org/1999/xlink"
-								aria-hidden="true"
-								role="img"
-								class="iconify iconify--material-symbols"
-								width="20"
-								height="20"
-								preserveAspectRatio="xMidYMid meet"
-								viewBox="0 0 24 24"
-								><path
-									fill="#444555"
-									d="M7 22q-.825 0-1.412-.587Q5 20.825 5 20q0-.825.588-1.413Q6.175 18 7 18t1.412.587Q9 19.175 9 20q0 .825-.588 1.413Q7.825 22 7 22Zm10 0q-.825 0-1.412-.587Q15 20.825 15 20q0-.825.588-1.413Q16.175 18 17 18t1.413.587Q19 19.175 19 20q0 .825-.587 1.413Q17.825 22 17 22ZM3.625 17L6.6 11.6L3 4H1V2h3.25l.95 2h16.5l-4.975 9H8.1L7 15h12v2Z"
-								/></svg
-							>SHOP
-						</div>
+						<div class="dropbtn">SHOP ⯆</div>
 						<div class="dropdown-content">
-							<a sveltekit:prefetch href="/">SHOP ØL</a>
+							<a sveltekit:prefetch href="/">ØL-SHOP</a>
 							<a sveltekit:prefetch href="/">KØB GAVEKORT</a>
 						</div>
 					</div>
@@ -138,7 +154,7 @@
 
 				<li>
 					<div class="dropdown">
-						<div class="dropbtn">VORES ØLUDVALG</div>
+						<div class="dropbtn">VORES ØLUDVALG ⯆</div>
 						<div class="dropdown-content">
 							<a sveltekit:prefetch href="/">NØRREBRO BRYGHUS</a>
 							<a sveltekit:prefetch href="/">BRAW</a>
@@ -150,7 +166,7 @@
 
 				<li>
 					<div class="dropdown">
-						<div class="dropbtn">EVENTS & SELSKABER</div>
+						<div class="dropbtn">EVENTS & SELSKABER ⯆</div>
 						<div class="dropdown-content">
 							<a sveltekit:prefetch href="/">EVENTKALENDER</a>
 							<a sveltekit:prefetch href="/">SELSKABER & MØDELOKALER</a>
@@ -160,7 +176,7 @@
 
 				<li>
 					<div class="dropdown">
-						<div class="dropbtn">OM BRYGHUSET</div>
+						<div class="dropbtn">OM BRYGHUSET ⯆</div>
 						<div class="dropdown-content">
 							<a sveltekit:prefetch href="/">VORES HISTORIE</a>
 							<a sveltekit:prefetch href="/">BÆREDYGTIGHED</a>
@@ -173,7 +189,7 @@
 
 				<li>
 					<div class="dropdown">
-						<div class="dropbtn">FOR VIRKSOMHEDER</div>
+						<div class="dropbtn">FOR VIRKSOMHEDER ⯆</div>
 						<div class="dropdown-content">
 							<a sveltekit:prefetch href="/">FIRMAEVENTS</a>
 							<a sveltekit:prefetch href="/">BOOK ET MØDELOKALE</a>
@@ -194,25 +210,20 @@
 	}
 
 	ul {
-		padding-block: 4rem;
+		padding-block: 3rem;
 		position: relative;
 		margin-right: 0.3rem;
 		height: 3em;
 		display: flex;
-		justify-content: space-around;
+		justify-content: space-between;
 		align-items: center;
 		list-style: none;
-		background-size: contain;
 	}
 
 	ul img {
 		width: 5rem;
 	}
 
-	.desktop-menu li {
-		position: relative;
-		height: 100%;
-	}
 	nav a {
 		display: flex;
 		height: 100%;
@@ -245,11 +256,6 @@
 		text-decoration: underline;
 	}
 
-	.hamburger-menu li {
-		padding: 1rem;
-		color: black;
-	}
-
 	#menu__toggle {
 		opacity: 0;
 	}
@@ -275,7 +281,7 @@
 		align-items: center;
 		position: fixed;
 		top: 20px;
-		right: 20px;
+		right: 40px;
 
 		width: 26px;
 		height: 26px;
@@ -312,6 +318,14 @@
 		padding-top: 0.5rem;
 	}
 
+	.hamburger-menu ul {
+		justify-content: start;
+	}
+
+	.hamburger-menu li {
+		padding-block: 1.3rem;
+	}
+
 	.menu__box {
 		display: flex;
 		flex-direction: column;
@@ -320,28 +334,19 @@
 		top: 0;
 		right: -100%;
 		z-index: 90;
-
 		width: 100%;
 		height: 100%;
-
 		margin: 0;
-		padding: 125px 0;
-
+		padding: 0;
 		list-style: none;
-
-		background-color: white;
+		background-color: rgb(168, 212, 221);
 		box-shadow: 1px 0px 6px rgba(0, 0, 0, 0.2);
-
 		transition-duration: 0.25s;
 	}
 
 	@media (max-width: 600px) {
 		.desktop-menu {
 			display: none;
-		}
-
-		a:hover {
-			color: #b12222;
 		}
 	}
 
@@ -351,19 +356,20 @@
 		}
 	}
 
+	/******************DROPDOWN***************/
+
 	.dropdown {
 		position: relative;
 		display: inline-block;
 	}
 
 	.dropdown-content {
-		text-align: center;
+		text-align: left;
 		display: none;
 		position: absolute;
 		background-color: rgb(255, 255, 255);
 		width: auto;
 		padding-inline: 0.5rem;
-		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
 		z-index: 1;
 	}
 
--- src/routes/Button.svelte
+++ src/routes/Button.svelte
@@ -1,13 +1,8 @@
 <script>
 	// @ts-nocheck
-
 	export let link;
 	import { page } from '$app/stores';
 </script>
-
-<!-- <a class="button" href={link}><slot /></a>
-
-<br /> -->
 
 <a class="btn" href={link}>
 	<svg>
@@ -16,40 +11,7 @@
 	<slot />
 </a>
 
-<!-- <br />
-
-<button><slot /></button>
-
-<br />
-
-<div class="scene">
-	<div class="cube">
-		<span class="side top">Se mere</span>
-		<span class="side front"><slot /></span>
-	</div>
-</div>
-
-<br /> -->
 <style>
-	.button {
-		font-size: 1em;
-		color: white;
-		border: 1px solid white;
-		background: #e3861b;
-		outline: solid #e3861b 2px;
-		padding: 0.25em 1em;
-		text-decoration: none;
-		transition: ease 0.5s;
-	}
-
-	.button:hover {
-		background-color: black;
-		transition: ease 0.5s;
-		outline: solid black 2px;
-	}
-
-	/******KNAP 2******/
-
 	* {
 		box-sizing: border-box;
 	}
@@ -88,82 +50,5 @@
 		stroke-dashoffset: 183;
 		transition: 1s;
 		stroke: black;
-	}
-
-	/*******KNAP 3******/
-
-	button {
-		position: relative;
-
-		background: #e3861b;
-		border: 1px solid white;
-		padding-inline: 15px;
-		padding-block: 10px;
-		font-size: 0.9em;
-		color: white;
-		box-shadow: 4px 4px 0px 0px white;
-		font-family: 'Open Sans', sans-serif;
-		font-weight: 700;
-		letter-spacing: 5px;
-		text-transform: uppercase;
-		transition: all 300ms ease-in-out;
-		left: 0;
-		top: 0;
-	}
-
-	button:hover {
-		left: 4px;
-		top: 4px;
-		box-shadow: 0 0 0 0 white;
-	}
-
-	/******KNAP 4*******/
-
-	.scene {
-		perspective: 1000;
-		width: 10em;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		margin-left: -5em;
-	}
-
-	.cube {
-		color: #ccc;
-		cursor: pointer;
-		font-family: 'Roboto', sans-serif;
-		transition: all 0.85s cubic-bezier(0.17, 0.67, 0.14, 0.93);
-		transform-style: preserve-3d;
-		transform-origin: 100% 50%;
-		width: 10em;
-		height: 2em;
-	}
-
-	.cube:hover {
-		transform: rotateX(-90deg);
-	}
-
-	.side {
-		box-sizing: border-box;
-		position: absolute;
-		display: inline-block;
-		height: 4em;
-		width: 10em;
-		text-align: center;
-		text-transform: uppercase;
-		padding-top: 1.5em;
-	}
-
-	.top {
-		background: #fff;
-		color: #222229;
-		transform: rotateX(90deg) translate3d(0, 0, 2em);
-	}
-
-	.front {
-		background: #e3861b;
-		color: #fff;
-		box-shadow: inset 0 0 0 5px #fff;
-		transform: translate3d(0, 0, 2em);
 	}
 </style>
