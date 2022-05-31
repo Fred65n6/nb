5b8a4d6f325cc7d9b90d3d147c0e4829dfc53aa5
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-05-31 18:43:56 +0200

    mobil

--- src/routes/Cookie_banner.svelte
+++ src/routes/Cookie_banner.svelte
@@ -4,18 +4,18 @@
 </script>
 
 <div class={current === 'cookie' ? 'selected' : ''} on:click={() => (current = 'cookie')}>
-	<div class="fixed w-full grid bottom-0 right-0 z-50 cookie">
-		<div class="relative p-4 bg-black/80 ">
+	<div class="fixed w-full grid bottom-0  right-0 z-50 cookie">
+		<div class="relative p-4 m-4 bg-black/80 ">
 			<div class="md:flex grid justify-between items-center ">
-				<div class="w-full md:w-auto mb-4 lg:mb-0">
+				<div class="w-full md:w-auto mb-4 lg:mb-0 mx-8">
 					<h3 class="mb-2 text-xl font-bold font-heading text-white">Vi bruger cookies</h3>
 					<p class="max-w-xs text-gray-200 leading-loose text-sm">
 						Vi bruger cookies for at forbedre brugeroplevelsen på vores site.
 					</p>
 				</div>
-				<div class="w-full md:w-1/3 px-4 flex">
+				<div class="w-full md:w-1/3 md:px-4 flex place-center pr-16">
 					<a
-						class="inline-flex ml-auto items-center px-8 py-4 mr-2 text-lg text-white hover:text-gray-100 font-light underline"
+						class="inline-flex ml-auto items-center px-8 md:py-4 md:mr-2 text-lg text-white hover:text-gray-100 font-light underline"
 						href="/"
 					>
 						<span>AFSLÅ</span>
