e2e048a48d9294145a9094668ba637dbbdd0f107
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-05-31 19:06:26 +0200

    mobil

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -61,7 +61,7 @@
 				<input type="Dato" placeholder="anders@gmail.com" />
 			</div>
 		</div>
-		<div class="md:flex grid gap-4">
+		<div class="md:flex pt-4 md:pt-0 grid gap-4">
 			<div class="">
 				<p>Dit tlf. nummer</p>
 				<input type="Dato" placeholder="23249020" />
